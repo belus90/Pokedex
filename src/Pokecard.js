1e1c32575cb95ecd6f0e24f271e48336543f07d8
belus90 <belus90@gmail.com>
2022-06-21 16:29:39 +0100

    Pokemon Cards Game finish

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -18,7 +18,9 @@ class Pokecard extends Component{
         return (
             <div className = "Pokecard">
                 <h1 classname = "Pokecard-title">{this.props.name}</h1>
+                <div className = "Pokecard-img">
                 <img src={imgSrc} alt={this.props.name} />
+                </div>
                 <div className = "Pokecard-data">Type: {this.props.type}</div>
                 <div className = "Pokecard-data">EXP: {this.props.exp}</div>
             </div>
