8f287598139c99f04d745b568d013d3df3b45050
belus90 <belus90@gmail.com>
2022-06-21 15:35:31 +0100

    Pokemon Cards with an extra function

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,19 +1,26 @@
 import { render } from "@testing-library/react";
 import './Pokecard.css';
 import React, {Component} from "react";
-const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+// const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
+
+// write a function wich helps us to make the "id" three digit
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
+//pedToThree takes a number, if the number is greater or equal than 999 return the number
+//othervise adding 00 to number and use "slice" to cut it for 3digit
+// exp.: 0023 => 023, 002 => 002 cut the last 3digit
 
 
 class Pokecard extends Component{
     render() {
-        let imgSrc = `${POKE_API}${this.props.id}.png`
+        let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
         return (
             <div className = "Pokecard">
-                <h1>{this.props.name}</h1>
+                <h1 classname = "Pokecard-title">{this.props.name}</h1>
                 <img src={imgSrc} alt={this.props.name} />
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.base_experience}</div>
+                <div className = "Pokecard-data">Type: {this.props.type}</div>
+                <div className = "Pokecard-data">EXP: {this.props.exp}</div>
             </div>
         )
     }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Pokecard from './Pokecard';
+import './Pokedex.css';
 
 class Pokedex extends Component{
 
@@ -20,10 +21,12 @@ class Pokedex extends Component{
     render(){
         return (
             <div className = "Pokedex">
-                <h1>POKEDEX!!</h1>
+                <h1 className = "Pokedex-name">POKEDEX!!</h1>
+                <div className = "Pokedex-cards">
                 {this.props.pokemon.map((p) => (
                     <Pokecard id = {p.id} name = {p.name} type = {p.type} exp = {p.base_experience} />
                 ))}
+                </div>
             </div>
         )
 
